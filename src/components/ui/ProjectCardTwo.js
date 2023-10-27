2c20ad55717b0c824b585c6fb0e8f9d2651cd253
Brian Luciano <brianavendanoluciano@gmail.com>
2023-10-27 11:37:27 +0800

    fix photo

--- src/components/ui/ProjectCardTwo.js
+++ src/components/ui/ProjectCardTwo.js
@@ -3,7 +3,7 @@ export function ProjectCardTwo(props) {
     return (
         <div class="w-full max-w bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
     <a href="#">
-        <img class="p-8 rounded-t-lg" src={image} alt="product image" />
+        <img class="w-full rounded-t-lg" src={image} alt="product image" />
     </a>
     <div class="px-5 pb-5">
         <a href="#">
